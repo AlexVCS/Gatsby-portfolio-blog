7cf51b3568c23ebeb115106c6e51307c54c65c33
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-09-17 17:58:26 -0400

    Adding starter to portfolio-page.js

--- src/templates/portfolio-page.js
+++ src/templates/portfolio-page.js
@@ -1,3 +1,13 @@
 import React from 'react'
 
 import Layout from "../components/layout"
+
+const Content = () => {
+  return (
+    <div>
+      React is cool, more portfolio jazz to come
+    </div>
+  )
+}
+
+export default Content
